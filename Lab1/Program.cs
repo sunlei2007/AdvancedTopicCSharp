dedb73ed306ed3a0113d210708678f00c7951fcd
Lei Sun <139188566@qq.com>
2023-03-22 14:02:46 -0500

    CreateMain

---
+++ Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿StringOperate op1 = new StringOperate((word, num) =>
+{
+    return string.Concat(Enumerable.Repeat(word, num));
+});
+Console.WriteLine(op1("hello", 2));
+
+StringOperate op2 = new StringOperate((word, num) =>
+{
+    return word.Remove(word.Length - num);
+});
+Console.WriteLine(op2("hello", 2));
+
+delegate string StringOperate(string wordArg, int numArg);
